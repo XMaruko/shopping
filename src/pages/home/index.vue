edb172dbb87ba87ca4b4559a0a082b46cebf06e4
ppmaruko <ppmaruko@163.com>
2019-12-13 15:05:53 +0800

    轮播图

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="app">
+    <!-- 搜索栏 -->
    <div class="search">
      <div class="search-bar">
        <icon type="search" size="15"/>
        <span>搜索</span>
      </div>
    </div>
+   <!-- 轮播图 -->
+   <swiper :autoplay="autoplay" :indicatorDots="indicatorDots" :circular="circular">
+     <swiper-item v-for="(item,index) in imgUrls" :key="index">
+       <image :src="item" class="slide-image"/>
+     </swiper-item>
+   </swiper>
   </div>
 </template>
 
@@ -14,7 +21,14 @@
     name: 'app',
     data () {
       return {
-
+        imgUrls: [
+          'https://f10.baidu.com/it/u=1261817652,2420085224&fm=72',
+          'http://img4.imgtn.bdimg.com/it/u=1540229220,2046462304&fm=26&gp=0.jpg',
+          'http://img1.imgtn.bdimg.com/it/u=1405017238,2507623329&fm=26&gp=0.jpg'
+        ],
+        indicatorDots: true, // 显示底部小圆点
+        autoplay: true,       // 自动播放
+        circular: true        // 是否采用衔接滑动
       }
     }
   }
@@ -39,5 +53,8 @@
   .search .search-bar span {
     font-size: 14px;
     color: #CCCCCC;
+  }
+  .slide-image {
+    width: 100%;
   }
 </style>
