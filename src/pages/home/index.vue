49659234d11d88e64ead46c084090a9a61a7b743
ppmaruko <ppmaruko@163.com>
2019-12-13 11:55:40 +0800

    完成搜索框

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="app">
-    
+   <div class="search">
+     <div class="search-bar">
+       <icon type="search" size="15"/>
+       <span>搜索</span>
+     </div>
+   </div>
   </div>
 </template>
 
@@ -9,11 +14,30 @@
     name: 'app',
     data () {
       return {
-        
+
       }
     }
   }
 </script>
 
-<style>
+<style scoped>
+  .search {
+    padding: 10px;
+    background-color: #EB4450;
+  }
+  .search .search-bar {
+    background-color: #fff;
+    width: 100%;
+    height: 30px;
+    border-radius: 3px;
+    text-align: center;
+  }
+  .search .search-bar icon {
+    vertical-align: middle;
+    width: 25px;
+  }
+  .search .search-bar span {
+    font-size: 14px;
+    color: #CCCCCC;
+  }
 </style>
